9c04f003fae4fcc95464e8a540f040dcd8d52dfc
AlistairArk <AlistairArk@Hotmail.com>
2020-03-12 20:40:24 +0000

    Rectified mouse rotation issue / Player Start location. Set inspector labels and triggers to all easy enable and disable of prefab generated mazes vs raw maze generation.
    Rectified mouse rotation issue - Implemented new MouseLook script in order to fix the issue causing "camera stutter" and "jumps" on mouse rotation.
    Player Start location - Fixed issue preventing player object from being moved to the start location on startup and the issue preventing the player object from being moved on coming into contact with the goal. The "Character Controller" component was found to be causing the issue and must be disabled between player object transforms.

---
+++ Maze Game/Assets/Scripts/CubeRotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotate : MonoBehaviour {
+
+	float rotationSpeed = 50f;
+
+    void FixedUpdate(){
+    	gameObject.transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+
+}
---
+++ Maze Game/Assets/Scripts/GoalDetector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalDetector : MonoBehaviour{
+
+	public GameObject mapGen;
+	private wallPlacer WallPlacer;
+
+
+    // Start is called before the first frame update
+    void Start(){
+        WallPlacer   = mapGen.GetComponent<wallPlacer>();
+        WallPlacer.startMap();
+    }
+
+
+
+
+	void OnTriggerEnter(Collider other){
+        WallPlacer.resetMap();
+        // WallPlacer
+        // foreach (ContactPoint contact in collision.contacts)
+        // {
+        //     Debug.DrawRay(contact.point, contact.normal, Color.white);
+        // }
+        // if (collision.relativeVelocity.magnitude > 2)
+        //     audioSource.Play();
+    }
+}
