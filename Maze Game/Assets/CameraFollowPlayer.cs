9c0093bef1a194f29800f3be3a83d41f593ac1ee
AlistairArk <AlistairArk@Hotmail.com>
2020-03-12 21:20:54 +0000

    Increased resolution of player map. Moved player map to canvas.

---
+++ Maze Game/Assets/CameraFollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour{
+    public Transform target;
+    public Vector3 targetOffset;
+
+    void Start(){
+        targetOffset = transform.position - target.position;
+    }
+    void Update(){
+
+        if (target){
+            transform.position = Vector3.Lerp(transform.position, target.position+targetOffset, 0.1f);
+        }
+    }
+}
