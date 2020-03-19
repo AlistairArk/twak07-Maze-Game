842e04df9077a4a8721a2f122b6ee5e531b01aa2
AlistairArk <AlistairArk@Hotmail.com>
2020-03-19 01:37:29 +0000

    Added earth model to project

--- Maze Game/Assets/MenuCameraRotation.cs
+++ Maze Game/Assets/MenuCameraRotation.cs
@@ -2,16 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MenuCameraRotation : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class MenuCameraRotation : MonoBehaviour{
+
+    public bool rotate = true;
+    public float rotationSpeed = .01f;
 
     // Update is called once per frame
     void FixedUpdate(){
-        transform.Rotate(0f,.01f,0f);
+        if (rotate) transform.Rotate(0f,rotationSpeed,0f);
     }
 }
