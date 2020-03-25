2a0e7b52913b118188478009385b28656d06ca91
AlistairArk <AlistairArk@Hotmail.com>
2020-03-25 18:21:49 +0000

    Implimented sliding door script

--- Maze Game/Assets/Scripts/Doorway.cs
+++ Maze Game/Assets/Scripts/Doorway.cs
@@ -2,17 +2,76 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Doorway : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Doorway : MonoBehaviour {
+
+    public bool doorOpen = false;
+    public bool doorLastState = false; // The status of the door at last update
+
+    /*
+    doorStatus
+        0 - Door is in a fixed position
+        1 - Door is closing
+        2 - Door is opening
+    */
+    public int doorStatus = 0;
+    public int animSpeed = 1;   // Speed at which the door opens and closes.
+
+    public GameObject terminalA;
+    public GameObject terminalB;
+
+    public GameObject doorA;
+    public GameObject doorB;
+
+    public Vector3 openStateA; // Open state vectors for door A
+    public Vector3 openStateB; // Open state vectors for door B
+
+    public Vector3 closedStateA; // Open state vectors for door A
+    public Vector3 closedStateB; // Open state vectors for door B
+
+    void Start(){
+        closedStateA = doorA.transform.localPosition;
+        closedStateB = doorB.transform.localPosition;
     }
 
+
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+
+        if (doorStatus==1){
+            OpenAnim();
+        }else if(doorStatus==2){
+            CloseAnim();
+        }else{
+            if (doorOpen&&!doorLastState) doorStatus=1;  // Door OPEN has been triggered
+            else if (!doorOpen&&doorLastState) doorStatus=2; // Door CLOSE has been triggered
+        }
+
+        doorLastState = doorOpen;
+    }
+
+    void CloseAnim(){
+
+        // Move our position a step closer to the target.
+        float step =  animSpeed * Time.deltaTime; // calculate distance to move
+        doorA.transform.localPosition = Vector3.Lerp(doorA.transform.localPosition, openStateA, step);
+        doorB.transform.localPosition = Vector3.Lerp(doorB.transform.localPosition, openStateB, step);
+
+        // Check if the position of the door and targer position
+        if (Vector3.Distance(doorA.transform.localPosition, openStateA) < 0.01f 
+            && Vector3.Distance(doorB.transform.localPosition, openStateB) < 0.01f)
+            doorStatus = 0;
+    }
+
+    void OpenAnim(){
+
+        // Move our position a step closer to the target.
+        float step =  animSpeed * Time.deltaTime; // calculate distance to move
+        doorA.transform.localPosition = Vector3.Lerp(doorA.transform.localPosition, closedStateA, step);
+        doorB.transform.localPosition = Vector3.Lerp(doorB.transform.localPosition, closedStateB, step);
+
+        // Check if the position of the door and targer position
+        if (Vector3.Distance(doorA.transform.localPosition, closedStateA) < 0.01f 
+            && Vector3.Distance(doorB.transform.localPosition, closedStateB) < 0.01f)
+            doorStatus = 0;
     }
 }
