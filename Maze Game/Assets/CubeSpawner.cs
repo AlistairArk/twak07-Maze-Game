280b3ec1862d376e3a6e417dee0405b974736e32
AlistairArk <AlistairArk@Hotmail.com>
2020-03-25 17:14:44 +0000

    Implimented "cubefield" scene space for hacking minigame

---
+++ Maze Game/Assets/CubeSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour{
+
+    public GameObject cube;
+    public int spawnCount = 40;
+
+    // Start is called before the first frame update
+    void Start(){
+        int i=0;
+        while (i!=spawnCount){
+            i++;
+            // CreatePrefab(cube, MazeGlobals.cellDoorParent, new Vector3(X+.8f, 0f, Z+.4f), new Vector3(1,1,1), 270);
+            // cube.transform.localScale = new Vector3(scale.x,scale.y,scale.z);
+            // int mapScale = MazeGlobals.mapScale;
+
+            GameObject newCube;
+            newCube = Instantiate(cube); // , new Vector3(pos.x*mapScale, pos.y, pos.z*mapScale), Quaternion.Euler(0,rotation,0));
+            newCube.transform.parent = gameObject.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+        
+
+}
---
+++ Maze Game/Assets/FallingCube.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingCube : MonoBehaviour{
+
+
+    Vector3 rotationAxis;
+
+    int maxHeight = 100;   // Cube - Max spawn hight - On initialization
+    int minHeight =-100;   // Cube - Min spawn hight - On initialization
+    int gridX = 50;        // Size of the map grid - X
+    int gridZ = 50;        // Size of the map grid - Z
+    
+
+    float size=50f;         // Size of the cubefield
+    float perimeter=3f;     // Gap between cubefield and the platform 
+    int deadZone = -100;    // Point at whcich the cube respawns
+
+    public float speed=0.5f;// Speed at which the cube will rotate
+    public Vector3 pos;     // Position of the cube
+    public Vector3 rot;     // Vector in which the cubes will rotate
+
+    // Start is called before the first frame update
+    void Start(){
+        // rotationAxis = new Vector3(,,);   
+        int chance = Random.Range(0, 4);
+        
+        float spawnX = 0;
+        float spawnZ = 0;
+
+        // Select side of platform from which to spawn the cube
+        if (chance==0){  // North field
+            spawnX = Random.Range(-size, gridX+size);
+            spawnZ = Random.Range(gridZ+perimeter, size+gridZ+perimeter);
+
+        }else if(chance==1){ // East Field
+            spawnX = Random.Range(gridX+perimeter, gridX+size+perimeter);
+            spawnZ = Random.Range(-size, size+gridZ);
+
+        }else if(chance==2){ // South Field
+            spawnX = Random.Range(-size, gridX+size);
+            spawnZ = Random.Range(-size-perimeter, -perimeter);
+
+        }else if(chance==3){ // West field
+            spawnX = Random.Range(-size-perimeter, -perimeter);
+            spawnZ = Random.Range(-size, gridZ+size);
+        }
+
+        // Set random position of the cube
+        Vector3 position = new Vector3(spawnX, (float)Random.Range(maxHeight, minHeight), spawnZ); 
+        transform.localPosition = position;
+
+        // Set random rotation vector
+        rot = new Vector3(Random.Range(0f, 1f),Random.Range(0f, 1f),Random.Range(0f, 1f));
+    }
+
+    // Update is called once per frame
+    void Update(){
+        pos = transform.localPosition;
+
+        // Move back to top
+        if (pos.y<deadZone) transform.localPosition = new Vector3(pos.x, maxHeight, pos.z);
+
+        // Rotate cube
+        transform.Rotate(rot * speed * Time.deltaTime);
+    }
+}
